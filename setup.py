b13d4633a968e75c739eb63fe76d6a91c6282786
Wesley Steve <wesley.silva23@hotmail.com>
2022-03-14 11:03:04 -0300

    removido o extras do setup

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=ler("requirements.txt"),
-    extras_require={"dev": ler("requirements-dev.txt")}
+    
 )
